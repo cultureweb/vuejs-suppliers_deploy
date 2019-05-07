33d97683ab91965213d610bebbb1a5fa03c8fae9
cultureweb <35487627+cultureweb@users.noreply.github.com>
2019-05-07 16:44:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './router/index.js'
 import * as VueGoogleMaps from 'vue2-google-maps'
-import apiKey from '../config.js'
 import { store }  from './store/store.js'
 import './registerServiceWorker'
 
